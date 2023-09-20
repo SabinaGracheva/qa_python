fd030728d495c48b8659de185a016c6ca0f81c0c
SabinaGracheva <grachevaqa@yandex.ru>
2023-09-20 22:50:49 +0300

    add tests file

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
+import pytest
 from main import BooksCollector
+
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -18,7 +20,150 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    @pytest.mark.parametrize(
+        'book_name',
+        [
+            '1',
+            'Название книги 26 симловов',
+            'В названии этой книги 40 символовввввввв'
+        ]
+    )
+    def test_add_new_book_three_books_successfully(self, book_name):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        assert 0 < len(collector.books_genre.values()) <= 40, \
+            'Книга не добавлена в словарь books_genre'
+
+    @pytest.mark.parametrize(
+        'book_name',
+        [
+            '',
+            'В названии этой книги 41 символлллллллллл'
+        ]
+    )
+    def test_add_new_book_name_book_0_and_41_characters(self, book_name):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        assert len(collector.books_genre.values()) == 0, \
+            'Книга не должна быть добавлена. Название книги отсутствует или превышает 40 символов'
+
+    @pytest.mark.parametrize(
+        'book_name, genre_name',
+        [
+            ['Семь смертей Эвелины Хардкасл', 'Детективы'],
+            ['Алладин', 'Мультфильмы'],
+            ['Дракула', 'Ужасы']
+        ]
+    )
+    def test_set_book_genre_for_book_successfully(self, book_name, genre_name):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre_name)
+        assert collector.books_genre[book_name] == genre_name, \
+            f'Жанр "{genre_name}" для книги "{book_name}" не установлен'
+
+    def test_set_book_genre_for_not_added_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Алые паруса')
+        collector.set_book_genre('Алладин', 'Мультфильмы')
+        assert 'Алладин' not in collector.books_genre, \
+            'Установлен жанр для не существующей книги'
+
+    def test_set_book_genre_no_genre_listed(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дракула')
+        collector.set_book_genre('Дракула', 'Триллеры')
+        assert 'Триллеры' not in collector.books_genre.values(), \
+            'Установлен не существующий жанр'
+
+    def test_get_book_genre_by_name(self):
+        collector = BooksCollector()
+        collector.add_new_book('Алладин')
+        collector.set_book_genre('Алладин', 'Мультфильмы')
+        assert collector.get_book_genre('Алладин') == 'Мультфильмы', \
+            'Жанр книги не соответствует её названию'
+
+    def test_books_with_genre_comedy(self):
+        collector = BooksCollector()
+        collector.add_new_book('Пигмалион')
+        collector.add_new_book('Алладин')
+        collector.add_new_book('Горе от ума')
+        collector.set_book_genre('Пигмалион', 'Комедии')
+        collector.set_book_genre('Алладин', 'Мультфильмы')
+        collector.set_book_genre('Горе от ума', 'Комедии')
+        assert 'Алладин' not in collector.get_books_with_specific_genre('Комедии'), \
+            'У книги "Алладин" жанр не соответствует "Комедии"'
+
+    def test_get_books_genre_successfully(self):
+        collector = BooksCollector()
+        collector.add_new_book('Десять негритят')
+        collector.add_new_book('Алладин')
+        collector.set_book_genre('Десять негритят', 'Детективы')
+        collector.set_book_genre('Алладин', 'Мультфильмы')
+        assert collector.get_books_genre() == {'Десять негритят': 'Детективы', 'Алладин': 'Мультфильмы'}, \
+            "Не все книги были добавлены"
+
+    @pytest.mark.parametrize(
+        'book_name, genre_name',
+        [
+            ['Чип и Дэйл', 'Мультфильмы'],
+            ['Горе от ума', 'Комедии'],
+            ['Дюна', 'Фантастика']
+        ]
+    )
+    def test_books_for_children_successfully(self, book_name, genre_name):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre_name)
+        assert collector.get_books_for_children() == [book_name], \
+            "Книга подходит для детей"
+
+    @pytest.mark.parametrize(
+        'book_name, genre_name',
+        [
+            ['Дракула', 'Ужасы'],
+            ['Семь смертей Эвелины Хардкасл', 'Детективы']
+        ]
+    )
+    def test_books_for_children_age_restrictions(self, book_name, genre_name):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre_name)
+        assert collector.get_books_for_children() == [], \
+            "Книга имеет возрастные ограничения и не подходит для детей"
+
+    def test_add_book_in_favorites_successfully(self):
+        collector = BooksCollector()
+        collector.add_new_book('Пигмалион')
+        collector.add_new_book('Алладин')
+        collector.add_book_in_favorites('Алладин')
+        assert collector.get_list_of_favorites_books() == ['Алладин']
+
+    def test_add_book_in_favorites_book_not_add(self):
+        collector = BooksCollector()
+        collector.add_new_book('Пигмалион')
+        collector.add_book_in_favorites('Дракула')
+        assert collector.get_list_of_favorites_books() != ['Дракула'], \
+            'Книга не может быть добавлена в "Избранное", так как она отсутствует в добавленных книгах'
+
+    def test_add_book_in_favorites_book_is_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Пигмалион')
+        collector.add_book_in_favorites('Пигмалион')
+        collector.add_book_in_favorites('Пигмалион')
+        favorites_books = collector.get_list_of_favorites_books()
+        assert favorites_books == ['Пигмалион'], \
+            "Книга уже добавлена в избранное"
+
+    def test_delete_book_from_favorites_successfully(self):
+        collector = BooksCollector()
+        collector.add_new_book('Пигмалион')
+        collector.add_book_in_favorites('Пигмалион')
+        collector.delete_book_from_favorites('Пигмалион')
+        favorites_books = collector.get_list_of_favorites_books()
+        assert favorites_books == [], "Книга не удалена из избранного"
